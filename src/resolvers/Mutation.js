840ad2600b4f4d3fc74454b1d10b3b06c1a9bd5c
Zoltan Toth <contact@zoltantoth.com>
2018-10-22 09:24:02 -0400

    Deleting items

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -9,6 +9,28 @@ const Mutations = {
 
     return item;
   },
+
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+
+    delete updates.id;
+
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id },
+      },
+      info,
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
