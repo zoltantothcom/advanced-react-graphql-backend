ba8a831821ab55be3a08fdf51ce090a294141fa6
Zoltan Toth <contact@zoltantoth.com>
2018-10-24 09:00:14 -0400

    Get currently logged in user with middleware and render props

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -7,6 +8,19 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
+
+// decode the JWT to get user ID
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto req for future requests to access
+    req.userId = userId;
+  }
+
+  next();
+});
 
 server.start(
   {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -4,6 +4,19 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if there is a currwnt user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info,
+    );
+  },
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
