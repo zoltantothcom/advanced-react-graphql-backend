e11d97a5022311793f5cf0eab6ad9cdbf8af6c26
Zoltan Toth <contact@zoltantoth.com>
2018-10-24 10:12:08 -0400

    Sign in form and custom error handling

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -59,6 +59,36 @@ const Mutations = {
     // return the user to the browser
     return user;
   },
+
+  async signin(parent, { email, password }, ctx, info) {
+    // check if there is a user with that email
+    const user = await ctx.db.query.user({
+      where: { email: email.toLowerCase() },
+    });
+
+    if (!user) {
+      throw new Error(`No user found for email ${email}`);
+    }
+
+    // check if the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('Invalid password!');
+    }
+
+    // generate the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+
+    // return the user
+    return user;
+  },
 };
 
 module.exports = Mutations;
